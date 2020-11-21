45f77cc2b986aa95786b1f6c6934be92cabce919
Kevin Chung <kchung@nyu.edu>
2020-11-21 17:24:09 -0500

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ ! -f "$FILE" ]; then
+if [ ! -f "/usr/bin/go" ]; then
     curl -O https://storage.googleapis.com/golang/go1.13.8.linux-amd64.tar.gz
     tar -xf go1.13.8.linux-amd64.tar.gz && mv go /usr/local
     ln -s /usr/local/go/bin/go /usr/bin/go
