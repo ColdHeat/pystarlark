a4137515377df6a187df930cdeb9e7bf8717f6ad
Kevin Chung <kchung@nyu.edu>
2020-11-21 18:10:23 -0500

    Fix redirection links

--- scripts/setup-linux.sh
+++ scripts/setup-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f "/usr/bin/go" ]; then
-    curl -vvv -O https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
+    curl -vvv -O https://dl.google.com/go/go1.15.5.linux-amd64.tar.gz
     tar -xf go1.15.5.linux-amd64.tar.gz
     mv go /usr/local
     ln -s /usr/local/go/bin/go /usr/bin/go
