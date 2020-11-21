efe7a96527c78c51e6b0db79a8b56f68bd78f804
Kevin Chung <kchung@nyu.edu>
2020-11-21 18:14:10 -0500

    Try symlinking elsewhere

--- scripts/setup-macos.sh
+++ scripts/setup-macos.sh
@@ -3,5 +3,5 @@ if [ ! -f "/usr/bin/go" ]; then
     curl -vvv -L -O https://golang.org/dl/go1.15.5.darwin-amd64.tar.gz
     tar -xf go1.15.5.darwin-amd64.tar.gz
     mv go /tmp
-    ln -s /tmp/go/bin/go /usr/bin/go
+    ln -s /tmp/go/bin/go /usr/local/bin/go
 fi
