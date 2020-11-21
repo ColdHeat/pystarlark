ff2cb3ecb2584823dca3715f21c12fa4155be365
Kevin Chung <kchung@nyu.edu>
2020-11-21 17:35:59 -0500

    Reduce wheel builds

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f "/usr/bin/go" ]; then
-    curl -O https://storage.googleapis.com/golang/go1.13.8.linux-amd64.tar.gz
+    curl -O https://storage.googleapis.com/golang/go1.13.8.linux-i686.tar.gz
     tar -xf go1.13.8.linux-amd64.tar.gz && mv go /usr/local
     ln -s /usr/local/go/bin/go /usr/bin/go
 fi
